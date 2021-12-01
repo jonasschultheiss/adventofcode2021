ac67f361c88f5a79dc62455b26540453b409e856
Jonas Schultheiss <32542628+jonasschultheiss@users.noreply.github.com>
2021-12-01 13:06:46 +0100

    removind dependency

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 "use strict";
 const { program } = require("commander");
 
-const challengeDictionary = {};
-
 program
   .description("A collection of solutions for the adventofcode 2021 event")
   .requiredOption("-d, --day <number>", "Selects the daily challenge")
